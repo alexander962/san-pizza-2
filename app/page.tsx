87cfb7a115f3c946f792c85960a58c0f690093e9
alexander962 <sanek12011996@gmail.com>
2026-01-22 18:49:27 +0300

    header refinement

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
 import { Header } from '@/components/shared/header';
 
 export default function Home() {
-  return (
-    <div>
-      <Header></Header>
-    </div>
-  );
+  return <div>Главная</div>;
 }
--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -1,6 +1,9 @@
 import { FC } from 'react';
 import { cn } from '@/lib/utils';
 import { Container } from './container';
+import Image from 'next/image';
+import { Button } from '@/components/ui';
+import { User } from 'lucide-react';
 
 interface Props {
   className?: string;
@@ -11,7 +14,21 @@ export const Header: FC<Props> = ({ className }) => {
     <header className={cn('border border-b', className)}>
       <Container className="flex items-center justify-between py-8">
         {/*Левая часть*/}
-        <div></div>
+        <div className="flex items-center gap-4">
+          <Image src="/logo.png" alt="Logo" width={35} height={35} />
+          <div>
+            <h1 className="text-2xl uppercase font-black">Next Pizza</h1>
+            <p className="text-sm text-gray-400 leading-3">вкусней уже некуда</p>
+          </div>
+        </div>
+
+        {/*Правая часть*/}
+        <div className="flex items-center gap-3">
+          <Button variant="outline" className="flex items-center gap-1">
+            <User size={16} />
+            Войти
+          </Button>
+        </div>
       </Container>
     </header>
   );
