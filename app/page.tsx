dedac713a0a37272936558e1e43994e26d1e818b
alexander962 <sanek12011996@gmail.com>
2026-01-23 08:56:29 +0300

    add new components

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
-import { Header } from '@/components/shared/header';
+import { Categories, Container, Title } from '@/components/shared';
 
 export default function Home() {
-  return <div>Главная</div>;
+  return (
+    <>
+      <Container className="mt-10">
+        <Title size="lg" text="Все пиццы" className="font-extrabold" />
+        <Categories />
+      </Container>
+    </>
+  );
 }
