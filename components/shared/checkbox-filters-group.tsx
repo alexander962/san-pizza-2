56bed5ad213edb85b478b6ab3b9c404dd7748f05
alexander962 <sanek12011996@gmail.com>
2026-03-03 07:44:30 +0300

    add search and showAll button

--- components/shared/checkbox-filters-group.tsx
+++ components/shared/checkbox-filters-group.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { FC } from 'react';
+import { ChangeEvent, FC, useState } from 'react';
 import { FilterCheckbox, FilterCheckboxProps } from './filter-checkbox';
 import { Input } from '@/components/ui';
 
@@ -9,7 +9,7 @@ type Item = FilterCheckboxProps;
 interface Props {
   title: string;
   items: Item[];
-  defaultItems?: Item[];
+  defaultItems: Item[];
   limit?: number;
   searchInputPlaceholder?: string;
   onChange: (values: string[]) => void;
@@ -27,14 +27,32 @@ export const CheckboxFiltersGroup: FC<Props> = ({
   onChange,
   defaultValue,
 }) => {
+  const [showAll, setShowAll] = useState(false);
+  const [searchValue, setSearchValue] = useState('');
+
+  const onChangeSearchInput = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(e.target.value);
+  };
+
+  const list = showAll
+    ? items.filter((item) => item.text.toLowerCase().includes(searchValue.toLowerCase()))
+    : defaultItems?.slice(0, limit);
+
   return (
     <div className={className}>
       <p className="font-bold mb-3">{title}</p>
-      <div className="mb-5">
-        <Input placeholder={searchInputPlaceholder} className="bg-gray-50 border-none" />
-      </div>
+      {showAll && (
+        <div className="mb-5">
+          <Input
+            value={searchValue}
+            onChange={onChangeSearchInput}
+            placeholder={searchInputPlaceholder}
+            className="bg-gray-50 border-none"
+          />
+        </div>
+      )}
       <div className="flex flex-col gap-4 max-h-96 pr-2 overflow-auto scrollbar">
-        {items.map((item, index) => (
+        {list.map((item, index) => (
           <FilterCheckbox
             key={index}
             text={item.text}
@@ -45,6 +63,13 @@ export const CheckboxFiltersGroup: FC<Props> = ({
           />
         ))}
       </div>
+      {items.length > limit && (
+        <div className={showAll ? 'border-t border-t-neutral-100 mt-4' : ''}>
+          <button onClick={() => setShowAll(!showAll)} className="text-primary mt-3">
+            {showAll ? 'Скрыть' : '+Показать все'}
+          </button>
+        </div>
+      )}
     </div>
   );
 };
